1afea634ad7364af56b62f8257b2c822304fcfc2
sami GHORBEL <sami.ghorbel@legalplace.fr>
2020-07-06 23:00:02 +0200

    WIp

--- src/utils/utils.js
+++ src/utils/utils.js
@@ -1,9 +1,4 @@
 import axios from 'axios';
 import { API_BOOKS_URL } from '../constants';
 
-const getBook = () => {
-  const sources = axios.get(`${API_BOOKS_URL}`);
-  return sources;
-};
-
-export default getBook;
+export const getBook = () => axios.get(`${API_BOOKS_URL}`);
